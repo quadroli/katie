aa104eff748b8a4c92d91058ce38091730b79728
mela <limisi@proton.me>
2025-08-09 13:26:09 +0300

    corrected typo

--- src/components/Message.js
+++ src/components/Message.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Message = () => {
   return (
     <div className="message-container">
-      <p>Happy Birthday Katie!! Thank you for being someone I can talk to, ever since we met you've always beena good friend. Ready to listen if I have summ to say. You've shown me kindness and I'm truly grateful. I'm wishing you all they joy in the world on this special day. Thank you for being you (:</p>
+      <p>Happy Birthday Katie!! Thank you for being someone I can talk to, ever since we met you've always been a good friend. Ready to listen if I have summ to say. You've shown me kindness and I'm truly grateful. I'm wishing you all they joy in the world on this special day. Thank you for being you (:</p>
     </div>
   );
 };
